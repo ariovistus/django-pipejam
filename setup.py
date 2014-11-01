d192a0517243f329ed8ca66f353d0d92bbdf3160
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2014-10-31 22:33:20 -0700

    add classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-pipejam',
-    version='0.0.1',
+    version='0.0.2',
     description='Dependency management on top of Django Pipeline.',
     author='Ellery Newcomer',
     author_email='ellery-newcomer@utulsa.edu',
@@ -17,6 +17,19 @@ setup(
         'django>=1.5',
         'django-pipeline>=1.3',
         'toposort>=1.1',
+    ],
+    url='http://github.com/ariovistus/django-pipejam',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
 
